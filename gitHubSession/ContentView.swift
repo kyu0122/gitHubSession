7371108abed83bc64b215b6b1c4424ddc29c75fd
Kyu Im <historyq12@gmail.com>
2024-04-18 20:00:48 +0900

    Initial Commit

---
+++ gitHubSession/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gitHubSession
+//
+//  Created by Kyu Im on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gitHubSession/gitHubSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubSessionApp.swift
+//  gitHubSession
+//
+//  Created by Kyu Im on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct gitHubSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
